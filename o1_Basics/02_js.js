732dc8d486df42c35adc690bed38b8772eaa1870
PREM RADHESHYAM MAHAJAN <161498035+Prem7218@users.noreply.github.com>
2024-02-29 18:59:41 +0000

    hello File Complete in some time.

---
+++ o1_Basics/02_js.js
@@ -0,0 +1,54 @@
+"use strict"; //treat all JS Code as Newer Version Of Js.
+
+// alert(3+3); // This work on web Html
+
+let score = 33
+let str = "33"
+console.log(typeof(str))
+console.log(typeof(score))
+/*
+// Convert String To Number by Using Explicit Conversion.
+let valueNumber = Number(str);
+console.log("If Value Is Number :- ")
+console.log(typeof(valueNumber));
+console.log(valueNumber);
+
+// If We Write (Unknow/ undefiend) Value In String At Time Of conversion Of Value String To Number <br>
+//Then It Shows Nan(Not a Number).
+
+str = "1234qwsdf"
+valueNumber = Number(str);
+console.log("If Value Is unknow :- ")
+console.log(typeof(valueNumber));
+console.log(valueNumber);
+
+// if We write null It shows value That Is 0. 
+
+str = undefined
+valueNumber = Number(str);
+console.log("If Value Is undefiend :- ")
+console.log(typeof(valueNumber));
+console.log(valueNumber);
+
+// if we Write bool value it's ans only 1 or 0.
+
+str = true
+valueNumber = Number(str);
+console.log("If Value Is undefiend :- ")
+console.log(typeof(valueNumber));
+console.log(valueNumber);
+
+// Convert Number TO Get Boolean Ans :- 
+*/
+str = 1;
+let convertToNumber = Boolean(str);
+console.log(convertToNumber)
+
+let Number = 55
+let stringNumber = String(Number)
+
+console.log(typeof(stringNumber))
+
+
+
+
---
+++ o1_Basics/form.js
@@ -0,0 +1,14 @@
+const accountNumber = 424005;
+let accountEmail = "Prem@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountNumber = 2
+accountEmail = "Prem@gmail.com"
+accountPassword = "12345678"
+accountCity = "Banglore"
+let accountState;
+
+ console.log(accountNumber);
+
+ console.table([accountNumber,accountEmail,accountPassword,accountCity,accountState])
