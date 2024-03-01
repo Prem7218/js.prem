497a15848e3d15613da0160660bbef254775390d
PREM RADHESHYAM MAHAJAN <161498035+Prem7218@users.noreply.github.com>
2024-03-01 16:56:30 +0000

    Memory_Allocation :-

---
+++ Memory_allocation.js
@@ -0,0 +1,14 @@
+// 1] Primitive data-Types :-
+//  All Primitive Type Get use Stack Memory.
+//  It Create Pass-By Value. [ Static ]
+
+let myYoutubeName = "PremMahajan@Funchod_News"
+
+let otherName = myYoutubeName
+
+console.log(otherName)
+
+// 2] Non-Primitive data-Types :-
+// All Non-Primitive Type Use Heap Or Stack Memory allcation.
+// It Creates Pass By Referance. [ Heap ]
+
