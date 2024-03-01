1abc8b3251809d67210faa506c20273b7f203265
PREM RADHESHYAM MAHAJAN <161498035+Prem7218@users.noreply.github.com>
2024-03-01 18:25:49 +0000

    All String Function

--- 02_Basics/My.js
+++ 02_Basics/My.js
@@ -4,5 +4,94 @@ const repoCount = 100
 // Console.log(name + repocount + "Value") This Syntax Is Not Good.
 
 // Below Is Good Writen Code Follow Below Syntax.
+// Called string InterPolation :- 
+
 console.log(`Hello My name is ${name} and my repo count is ${repoCount}`)
 
+const gameName = new String('Prem')
+
+console.log(gameName.at(2))
+
+console.log(gameName.anchor())
+
+console.log(gameName.big())
+
+console.log(gameName.blink())
+
+console.log(gameName.bold())
+
+console.log(gameName.charAt(2))
+
+console.log(gameName.charCodeAt(2))
+
+console.log(gameName.codePointAt(2))
+
+console.log(gameName.concat(" Radheshyam ","Mahajan"))
+
+console.log(gameName.fontcolor())
+
+console.log(gameName.fixed())
+
+console.log(gameName.fontsize())
+
+console.log(gameName.includes())
+
+console.log(gameName.indexOf('2'))
+
+console.log(gameName.italics())
+
+console.log(gameName.lastIndexOf('e'))
+
+console.log(gameName.link())
+
+console.log(gameName.match())
+
+console.log(gameName.matchAll())
+
+console.log(gameName.normalize())
+
+console.log(gameName.padEnd())
+
+console.log(gameName.padStart())
+
+console.log(gameName.repeat())
+
+console.log(gameName.replace())
+
+console.log(gameName.replaceAll())
+
+console.log(gameName.search('e'))
+
+console.log(gameName.slice(2))
+
+console.log(gameName.small())
+
+console.log(gameName.startsWith('P'))
+
+console.log(gameName.strike())
+
+console.log(gameName.sub())
+
+console.log(gameName.substr())
+
+console.log(gameName.substring(2))
+
+console.log(gameName.sup())
+
+console.log(gameName.toLocaleLowerCase())
+
+console.log(gameName.toLocaleUpperCase())
+
+console.log(gameName.toLowerCase())
+
+console.log(gameName.toUpperCase())
+
+console.log(gameName.toString())
+
+console.log(gameName.trim('     Prem '))
+
+console.log(gameName.trimEnd())
+
+console.log(gameName.trimStart())
+
+console.log(gameName.valueOf('e'))
