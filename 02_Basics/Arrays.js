7d9f54d129efd420e3e38adeac5c1ce3cb77a157
PREM RADHESHYAM MAHAJAN <161498035+Prem7218@users.noreply.github.com>
2024-03-03 16:45:36 +0000

    Arrays_File

---
+++ 02_Basics/Arrays.js
@@ -0,0 +1,31 @@
+const mh = ["thor", "IronMan", "Spideman"]
+const dch = ["superMan", "ManPower", "BatMan"]
+
+//mh.push(dch);
+
+//console.log(mh);
+//console.log(mh[3][0])
+//const allh = mh.concat(dch);
+///console.log(allh)
+
+// Spread Operator || Concat :-
+//const allnh = [...mh, ...dch]
+//console.log(allnh)
+
+// Get In Single Array Use Flat :-
+//const aay = [1,2,3,[4,5,6],7,[4,5,6],8,[4,5,6]]
+//const raay = aay.flat(Infinity)
+//console.log(raay)
+
+// Create Array Using Console Log By Refrance.
+//console.log(Array.isArray("Prem"))
+///console.log(Array.from("Prem"))
+
+console.log(Array.from({name: "Prem"})) // It Creates Ambiguty What Value Array Create Values Or Name Or Obj.
+
+// Direct Create Array Using Variable Name Of There Values.
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+// console.log(Array.of(score1,score2,score3))
+
