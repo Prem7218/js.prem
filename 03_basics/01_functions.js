a6eca94a242f2661738508053d39b1b9b50d09eb
PREM RADHESHYAM MAHAJAN <161498035+Prem7218@users.noreply.github.com>
2024-03-04 04:05:51 +0000

    Functions_js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+// Function Use For Particular Task To Repetative at any Location in code if it is in There Scope.
+
+// It We Use Parameter in Function But Not It Get added in The Calling Time Then It Gets Undefined vaule. 
+// function loginUser(user)
+// {
+//     return `${user} just logged in`
+// }
+
+// It Value Not Assign.
+// function loginUser(user)
+// {
+//     if(user === undefined)  // if(!user) Same as Above.
+//     {
+//         console.log("Please Enter a User Name...")
+//         return 
+//     }
+//     return `${user} just logged in`
+// }
+//console.log(loginUser())
+
