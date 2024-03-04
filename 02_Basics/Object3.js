b1c8327e2bce9c3e4ba53d340e675aad47e7aacb
PREM RADHESHYAM MAHAJAN <161498035+Prem7218@users.noreply.github.com>
2024-03-04 03:42:21 +0000

    Object3_API

---
+++ 02_Basics/Object3.js
@@ -0,0 +1,32 @@
+// API and Object :-
+
+const course = {
+    coursename: "Js in Hindi",
+    price: "999Rs",
+    courseInstructor: "Hitesh Sir"
+}
+// console.log(course["courseInstructor"])
+
+
+// D-Structure Object :-
+//const {coursename} = course
+//console.log(coursename)
+//const {coursename: cn} = course
+//console.log(cn)
+// const navbar = ({company}) => {
+
+// }
+// navbar(companyname = "Prem")
+
+// Json Look API :- 
+// Java Script Obj Notation :-
+// {
+//     "name": "Prem",
+//     "coursename": "Js In Hindhi",
+//     "price": "free"
+// }
+//  [
+//     {},
+//     {},
+//     {}
+//  ]
