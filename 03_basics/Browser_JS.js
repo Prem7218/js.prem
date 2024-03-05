c74211d0d9d2cc914f3c4ea81916460a5fd012fa
PREM RADHESHYAM MAHAJAN <161498035+Prem7218@users.noreply.github.com>
2024-03-05 05:12:31 +0000

    Browser_JS.js

---
+++ 03_basics/Browser_JS.js
@@ -0,0 +1,15 @@
+// Browser Basics :-
+// In Js Golbal Execution Contect always Create and Stored In [ this ].
+// [ Value Of This Is Window Object. ]
+// Execute In Single Thread Because Js Is Single Threded.
+
+// Second Is Fucntion Execution Contect.
+
+// Eval Is Property Of Global Object.
+// Third  Eval Execution Content. // Comes In Mongoes.
+
+// Js Code Run On Two Phases :-
+// 1st :- Memory Phase.
+// 2nd :- Execution Phase.
+
+
