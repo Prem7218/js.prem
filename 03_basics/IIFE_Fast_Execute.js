cc0cc57efa598e9be9bfc885f76996606b752072
PREM RADHESHYAM MAHAJAN <161498035+Prem7218@users.noreply.github.com>
2024-03-05 03:58:49 +0000

    IIFE_Fast.js

---
+++ 03_basics/IIFE_Fast_Execute.js
@@ -0,0 +1,24 @@
+// Immediately Invoked Function Expression (IIFE)
+
+// Suppose If We Try To Execute Any Function First At Time Of Program Execution Then Use [ IIFE ]. 
+
+// // Normal Function :-
+// function chai()
+// {
+//     console.log(`DB CONNECTED`);
+// }
+
+// The Polution Of Global Scope Which Was Creates Problem && The Variable In Globle Scope Which Also Creates Problem , Therefor For That Reason To Remove The Polution Of Gobal Scope We Are Use The IIFE Thechnique And At Lat Always Use Semicolon Of IIFE Function.
+
+// IIFE USED With Normal Function :- Name IIFE :-
+// (function chai()
+// {
+//     console.log(`DB CONNECTED`);
+// })();
+
+// IIFE USED With => Function :-
+
+// Normal IIFE :-
+// ((name) => {
+//     console.log(`DB CONNECTED TWO ${name}`);
+// })('Prem')
