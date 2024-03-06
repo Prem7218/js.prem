1b91c2b7340bec99085f4a9b71f9403c75a61bf0
PREM RADHESHYAM MAHAJAN <161498035+Prem7218@users.noreply.github.com>
2024-03-06 06:42:44 +0000

    Loops.js_File2

--- Loops.file/Loops_2.js
+++ Loops.file/Loops_2.js
@@ -19,7 +19,7 @@
 //     }
 // ]
 
-const myCoding = ['Js', 'Ruby', 'CPP', 'JAVA', "Go Long"]
+// const myCoding = ['Js', 'Ruby', 'CPP', 'JAVA', "Go Long"]
 
 // Without Return it Gets Undefind Because Its Refrance Is Only Between Function
 // But After Return he Gets Undefiend This Feature Not Provide By Js Developes.
@@ -29,3 +29,27 @@ const myCoding = ['Js', 'Ruby', 'CPP', 'JAVA', "Go Long"]
 // console.log(val)
 
 
+// Filter Is When We Get Specific Part Of Array.
+
+// { } Curly Brace Work As Scope.
+
+// First :- 
+const arr = [1,2,3,4,5,6,7,8,9,10]
+
+// const store = arr.filter((num) => num > 4)
+
+// Second :- 
+// Because of Curly Brace :- 
+// { } Curly Brace Work As Scope Then Use () Or return.
+
+// First Technique Using Return.
+
+// const store = arr.filter((num) => {
+//     return num > 4;
+// })
+
+// Second Using ().
+// Single Statment Note Use The { } Its ok.
+const store = arr.filter( (num) => (num > 4))
+
+console.log(store)
