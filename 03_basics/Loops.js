02fdc065e99d3c38b07ec0087f2f6b4398279d3d
PREM RADHESHYAM MAHAJAN <161498035+Prem7218@users.noreply.github.com>
2024-03-06 06:02:59 +0000

    Obj_Loops.js

---
+++ 03_basics/Loops.js
@@ -0,0 +1,42 @@
+// For of Loop :-
+// We Can Use This Loop On any One Obj Or string Or array's.
+// const arr = [1,2,3,4,6]
+
+// // Iterator is Var name:-
+// for (const iterator of arr) {
+//     console.log(iterator)
+// }
+
+// Using Loops :-
+// const greeting = "Hello World!"
+
+// for(const i of greeting)
+// {
+//     console.log(`Each Char is ${i}`)
+// }
+
+
+// We Can set any Value In map.
+const map = new Map()
+map.set(1,"Mumbai",)
+map.set(2,"Buldana")
+
+//console.log(map)
+
+// [ i, m ] is Destructure.
+// for(const [i, m] of map)
+// {
+//     console.log(i," ",m)
+// }
+
+// Object Is Not Itratable Using Loop.
+// const myObj = {
+//     "Game1": "Footboll",
+//     'Game2': "Batboll"
+// }
+
+// for(const [i, m] of myObj)
+// {
+//     console.log(i," ",m)
+// }
+
---
+++ 03_basics/ObjCore.js
@@ -0,0 +1,23 @@
+// For Itetrating Obj Use For I Loop.
+// const myObj = {
+//     js: "JavaScript",
+//     cpp: "C++",
+//     rb: "Ruby",
+//     swift: "Switf"
+// }
+
+// for(i in myObj)
+// {
+//     console.log(`${i} Shortcut is For ${myObj[i]}`)
+// }
+
+// For in also Work On array's.
+
+const arr = ['Js', 'Ruby', 'CPP', 'JAVA', "Go Long"]
+
+for(i in arr)
+{
+    console.log(`${i} Shortcut is For ${arr[i]}`)
+}
+
+// For in Loop Not Work On Map.
