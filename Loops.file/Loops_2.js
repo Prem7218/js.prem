9aa8b2b1cc9a5ea3e117382aac192eae02e4c93f
PREM RADHESHYAM MAHAJAN <161498035+Prem7218@users.noreply.github.com>
2024-03-06 06:31:06 +0000

    Change.LooP.file

---
+++ Loops.file/Loops_2.js
@@ -0,0 +1,31 @@
+// Loops :- 
+
+// const myCoding1 = [
+//     {
+//         lname: 'JavaScript',
+//         lfile: "JS"
+//     },
+//     {
+//        lname: "Java",
+//        lfile: "JAVA"
+//     },
+//     {
+//         lname: 'CPP',
+//         lfile: 'C++'
+//     },
+//     {
+//         lname: 'Python',
+//         lfile: 'Py'
+//     }
+// ]
+
+const myCoding = ['Js', 'Ruby', 'CPP', 'JAVA', "Go Long"]
+
+// Without Return it Gets Undefind Because Its Refrance Is Only Between Function
+// But After Return he Gets Undefiend This Feature Not Provide By Js Developes.
+// const val = myCoding.forEach((item) => {
+//     return item;
+// })
+// console.log(val)
+
+
