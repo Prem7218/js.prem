5d83d2e72c13d8732466b98284b357326f913192
PREM RADHESHYAM MAHAJAN <161498035+Prem7218@users.noreply.github.com>
2024-03-18 03:41:23 +0000

    Promise

--- Promices.js
+++ Promices.js
@@ -31,26 +31,56 @@
 // });
 
 
-const promiseFour = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        let error = false
-        if(!error)
-        {
-            resolve({username: 'Prem Mahajan', pass: "1221"})
-        }
-        else {
-            reject("Something is wrong")
-        }
-    }, 1000);
-});
+// const promiseFour = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         let error = true
+//         if(!error)
+//         {
+//             resolve({username: 'Prem Mahajan', pass: "1221"})
+//         }
+//         else {
+//             reject("Something is wrong")
+//         }
+//     }, 1000);
+// });
 
-promiseFour.then((user) => {
-    console.log(user);
-    return user.username;
-}).then((username) => {
-    console.log(username)
-}).catch((error) => {
-    console.log(error)
-});
+// promiseFour.then((user) => {
+//     console.log(user);
+//     return user.username;
+// }).then((username) => {
+//     console.log(username)
+// }).catch((error) => {
+//     console.log(error)
+// });
 
 
+// const promiseFive = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         let error = ture;
+//         if(!error) {
+//             resolve({
+//                 username: "Javascript", password: "123"
+//             });
+//         }
+//         else {
+//             reject('Error: Js Went Wrong...')
+//         }
+//     }, 1000);
+// });
+
+// async function consumePromiseFive() {
+//     const response = await promiseFive
+//     console.log(response);
+// }
+
+// async function consumePromiseFive() {
+//     try {
+//         const response = await promiseFive
+//         console.log(response)
+//     }
+//     catch(error) {
+//         console.log(error);
+//     }
+// }
+
+// consumePromiseFive();
